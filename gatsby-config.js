9527398175690515cb8ffef18591287c1f266406
Todd Campbell-Crow <creaux@gmail.com>
2022-02-03 17:34:05 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://crowbelldesign.gatsbyjs.io/`,
+        title: "Crowbell Design",
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `portfolio`,
+        path: `${__dirname}/portfolio`,
+      }
+    },
     ]
 }
