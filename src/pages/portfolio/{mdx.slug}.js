953c2be686f9bf93e80d55deabe061be8948be9a
Todd Campbell-Crow <creaux@gmail.com>
2022-02-04 17:13:39 -0500

    Pausing for the evening

---
+++ src/pages/portfolio/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+
+const PortfolioPost = () => {
+  return (
+    <Layout pageTitle="Super Cool Portfolio Examples">
+      <p>My portfolio contents will go here (eventually).</p>
+    </Layout>
+  )
+}
+
+export default PortfolioPost
